4e30c8cf814e583614000e1042befdfb2a8c055f
sanad <mohdsanadzakirizvi@gmail.com>
2017-09-26 02:59:57 +0530

    removed issue line from destination.controller.js

--- dikpala/client/scripts/controllers/destination.controller.js
+++ dikpala/client/scripts/controllers/destination.controller.js
@@ -1,5 +1,4 @@
 import { Controller } from 'angular-ecmascript/module-helpers';
-import { ActionSheetController } from 'ionic-angular';
 
 
 export default class DestinationCtrl extends Controller {
